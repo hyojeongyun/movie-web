f5db02874129f2d5c5ae6b091c20900a9cc1f491
Yun HyoJeong <63863627+hyojeongyun@users.noreply.github.com>
2021-08-28 21:59:15 +0900

    Initial commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'f489fd86cd63aea16488e96b3e8b241e';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
